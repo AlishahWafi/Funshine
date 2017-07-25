2d8ce693c2b5e33b8e4bf4aa6f02cc5799196676
AlishahWafi <alishahwafi@gmail.com>
2017-07-25 11:18:06 +0500

    Volley get request

--- app/src/main/java/com/techprober/funshine/WeatherActivity.java
+++ app/src/main/java/com/techprober/funshine/WeatherActivity.java
@@ -2,11 +2,21 @@ package com.techprober.funshine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class WeatherActivity extends AppCompatActivity {
 
     final String URL_BASE = "http://api.openweathermap.org/data/2.5/forecast";
-    final String URL_COORD = "/?lat=";
+    final String URL_COORD = "/?lat=9.9687&lon=76.299";
+    final String URL_UNITS = "&units=metric";
     final String API_KEY = "&APPID=7127ba644a3ef6ad20d80eecbe583ca0";
 
     @Override
@@ -14,6 +24,21 @@ public class WeatherActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
 
-        
+        final String url = URL_BASE + URL_COORD + URL_UNITS + API_KEY;
+
+        JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.v("FUN", "RES: " + response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.v("FUN", "Err: " + error.getLocalizedMessage());
+            }
+        });
+
+        Volley.newRequestQueue(this).add(jsonRequest);
     }
 }
